6baa75648abaf48fa3995c1f7d02363d04842b6e
Mcdimmy007 <101529739+Mcdimmy007@users.noreply.github.com>
2023-08-28 12:48:56 -0500

    Upload HW1

---
+++ Hw1.cs
@@ -0,0 +1,13 @@
+﻿namespace Wk1Hw1;
+class Program
+{
+    static void Main(string[] args)
+    {
+            double X = 2.5;
+            double Y = 3.3;
+
+            double Z = (4*X*X) + (3*Y);
+            Console.WriteLine($"X = {X}, Y = {Y}");
+            Console.WriteLine($"The Value of Z is: {Z}");
+        }
+}
